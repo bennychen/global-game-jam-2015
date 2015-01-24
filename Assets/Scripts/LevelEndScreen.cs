95002fb1c29e1c94813d55e67213daabef725ba2
Benny <rockerbenny@gmail.com>
2015-01-25 01:40:07 +0800

    Added ui flow and collision check for ghost&target

---
+++ Assets/Scripts/LevelEndScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEndScreen : MonoBehaviour 
+{
+	public static bool Success { get; set; }
+
+	public void Restart()
+	{
+		SaveManager.LoadCurrentLevel();
+	}
+
+	public void Next()
+	{
+		if (SaveManager.CurrentLevelIndex >= SaveManager.LevelCount)
+		{
+			Application.LoadLevel("FinishScreen");
+		}
+		else
+		{
+			SaveManager.AdvanceLevel();
+			SaveManager.LoadCurrentLevel();
+		}
+	}
+
+	private void OnEnable()
+	{
+
+	}
+}
