b48de5a636ba56cac9186f807e8a3435f54bd34b
Benny <rockerbenny@gmail.com>
2015-01-25 01:51:16 +0800

    Added collision event for wall block

---
+++ Assets/Scripts/WallBlock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBlock : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision collision)
+	{
+		for (int i = 0; i < collision.contacts.Length; i++)
+		{
+			ContactPoint contactPoint = collision.contacts[i];
+			if (contactPoint.otherCollider.CompareTag("Player"))
+			{
+				Debug.Log("player collider wall");
+				Player player = contactPoint.otherCollider.GetComponent<Player>();
+				break;
+			}
+		}
+	}
+}
