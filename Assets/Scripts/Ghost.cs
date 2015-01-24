fc833fa5e3f74abbd33d6f734a17be9bbf718344
Matthew <sssyeke@gmail.com>
2015-01-24 17:45:07 +0800

    鬼魂随机运动

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -10,15 +10,19 @@ public class Ghost : MonoBehaviour
 	}
 	void randomMove (float deltaTime){
 		if (randomMoveTimeCounter <= 0) {
-			randomMoveTimeCounter = 15;
+			randomMoveTimeCounter = randomMovePeriod;
 			//randomMove aciton
 			float y = Random.Range(randomMoveMin, randomMoveMax);
 			float x = Random.Range(randomMoveMin, randomMoveMax);
+			Vector3 movement = new Vector3 ( x,0,y);
+			transform.Translate(movement);
 
+		
 		} else {
 			randomMoveTimeCounter -= deltaTime;
 		}
 	}
+
 
 	// Update is called once per frame
 	void Update (){
@@ -42,12 +46,13 @@ public class Ghost : MonoBehaviour
 	
 	[SerializeField]
 	public float
-		randomMovePeriod = 15;
+		randomMovePeriod = 1;
 	
 	[SerializeField]
-	private Vector2 randomMoveSize = new Vector2(0, 3);
+	private Vector2 randomMoveSize = new Vector2(-0.3f, 0.3f);
 	
 	public float randomMoveMin { get{return randomMoveSize.x;}}
 	public float randomMoveMax { get{return randomMoveSize.y;}}
-	private float randomMoveTimeCounter = 15;
+	[SerializeField]
+	private float randomMoveTimeCounter = 0;
 }
