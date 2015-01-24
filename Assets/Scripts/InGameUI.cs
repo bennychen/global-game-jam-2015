632522bfdd30e131824d41c46b99329eddff717b
liuyuyefz <liuyuyefz@gmail.com>
2015-01-24 21:15:05 +0800

    main menu

---
+++ Assets/Scripts/InGameUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour {
+	
+	private void OnEnable () {
+		_startButton.onClick.AddListener (() =>
+		                                  {
+			Debug.Log("GoToLevel001");
+			Application.LoadLevel("Level001");
+		}); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	[SerializeField]
+	private Button _startButton;
+
+}
