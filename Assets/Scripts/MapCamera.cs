1e2233f94d21953132f901cae62a241ca2a5ffe4
liuyuyefz <liuyuyefz@gmail.com>
2015-01-25 13:49:52 +0800

    mini map and random sound

---
+++ Assets/Scripts/MapCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapCamera : MonoBehaviour {
+
+	//[SerializeField]
+	//public bool IsShowMiniMap = false; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//active=IsShowMiniMap;
+	}
+
+}
